0aa04486bb1403b6b3a835414ed95247f03cc2c4
cherrycollapse <cherrycollapse@gmail.com>
2022-09-22 16:48:46 +0300

    add classwork

--- src/step/learning/Main.java
+++ src/step/learning/Main.java
@@ -4,7 +4,7 @@ import step.learning.oop.Library;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // System.out.println("Hello world!");
         new Library().run();
     }
 }
