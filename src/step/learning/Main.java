613fd10b80b7fe17f7746012ca4f0051356e413d
cherrycollapse <cherrycollapse@gmail.com>
2022-10-03 02:07:42 +0300

    Annotations homework 2 (28.09)

--- src/step/learning/Main.java
+++ src/step/learning/Main.java
@@ -9,166 +9,159 @@ import step.learning.oop.Library;
 import java.io.File;
 import java.lang.reflect.Method;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.InputMismatchException;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
         // Определяем текущий класс
-        Class<?> currentClass = Main.class ;
+        Class<?> currentClass = Main.class;
         // Извлечь из имени класса имя пакета
-        String packageName = currentClass.getPackage().getName() ;
+        String packageName = currentClass.getPackage().getName();
         // получаем перечень имен классов из пакета и его под-пакетов
-        List<String> classNames = getClassNames(packageName ) ;
+        List<String> classNames = getClassNames(packageName);
 
-        if(classNames==null){
+        if (classNames == null) {
             System.out.println("Error scanning package");
             return;
         }
 
         // выбрать только те классы, которые аннотированы как DemoClass
         List<Class<?>> demoClasses = new ArrayList<>();
-        for( String className : classNames ) {
+        for (String className : classNames) {
             Class<?> theClass;
 
             try {
                 theClass = Class.forName(className);
-            }
-            catch(Exception ignored) { // ignored - спец имя для неиспользуемой переменнойй
+            } catch (Exception ignored) { // ignored - спец имя для неиспользуемой переменнойй
                 continue;
             }
 
-            if(theClass.isAnnotationPresent(DemoClass.class)){
+            if (theClass.isAnnotationPresent(DemoClass.class)) {
                 demoClasses.add(theClass);
             }
 
-           // System.out.println( className ) ;
+            // System.out.println( className ) ;
         }
 
-        System.out.println("Demo classes : ");
-        int i = 1;
+//        Д.З. Обеспечить повтор диалога (меню-выбор) до выбора выхода
+//        При формировании меню учесть приоритет, указанный в DemoClass аннотации
+//        Добавить вывод уведомления в случае если в классе не будет найден метод EntryPoint
 
-        for(Class<?> theClass : demoClasses){
-            System.out.printf("%d. %s %n",i++,theClass.getName());
-        }
+        int userChoice;
 
-        System.out.println( "0. Exit" ) ;
-        System.out.print("Make your choice: ");
-
-        // new DataTypes().Run();
-        // new Complex().Run();
-        // new Library().run();
-        // new SerializationDemo().run() ;
-        // new FilesDemo().run() ;
-        // new AnnotationsDemo().run() ;
-
-        Scanner kbScanner = new Scanner( System.in ) ;
-        int userChoice = -1;
-
-        try{
-            userChoice= kbScanner.nextInt() ;
-        }catch (InputMismatchException ignored){
-            System.out.println("Incorrect choice");
-        }
+        demoClasses.sort(new Comparator<>() {
+            public int compare(Class<?> obj1, Class<?> obj2) {
+                return Integer.compare(obj1.getAnnotation(DemoClass.class).priority(), obj2.getAnnotation(DemoClass.class).priority());
+            }
+        });
 
 
-        System.out.println("Choice :"+userChoice);
+        do {
+            System.out.println("Demo classes : ");
+            int i = 1;
 
-        if(userChoice == 0){
-            System.out.println("Exit...");
-            return;
-        }
-
-        int index = userChoice -1;
-        if(index>=demoClasses.size()){
-            System.out.println("Incorrect choice");
-        }
-        else{
-            Class<?> execClas = demoClasses.get(index);
-            // сканируем методы на аннотацию EntryPoint и запускаем первый попавшийся// }
-
-            Method[] methods = execClas.getDeclaredMethods();
-            for(Method method : methods){
-                if(method.isAnnotationPresent(EntryPoint.class)){
-
-                        try{
-                            method.invoke(execClas.getDeclaredConstructor().newInstance());
-                        }
-                        catch (Exception ex){
-                            System.out.println("Execution error : "+ex.getMessage());
-                        }
-                    }
-
-                    break;
-                }
+            for (Class<?> theClass : demoClasses) {
+                System.out.printf("%d. %s %n", i++, theClass.getName());
             }
 
-        }
+            System.out.println("0. Exit");
+            System.out.print("Make your choice: ");
+
+            Scanner kbScanner = new Scanner(System.in);
+            userChoice = -1;
+
+            try {
+                userChoice = kbScanner.nextInt();
+            } catch (InputMismatchException ignored) {
+                System.out.println("Incorrect choice");
+            }
 
 
+            System.out.println("Choice :" + userChoice);
 
-    private static List<String> getClassNames( String packageName ) {
+            if (userChoice == 0) {
+                System.out.println("Exit...");
+                return;
+            }
+
+            int index = userChoice - 1;
+            if (index >= demoClasses.size()) {
+                System.out.println("Incorrect choice");
+            } else {
+                Class<?> execClas = demoClasses.get(index);
+
+                Method[] methods = execClas.getDeclaredMethods();
+                for (Method method : methods) {
+                    if (method.isAnnotationPresent(EntryPoint.class)) {
+                        try {
+                            method.invoke(execClas.getDeclaredConstructor().newInstance());
+                        } catch (Exception ex) {
+                            System.out.println("Execution error : " + ex.getMessage());
+                        }
+                    }
+                    else{
+                        System.out.println("(Entry Point not present)");
+                    }
+                }
+            }
+            System.out.println();
+
+        }while(true);
+    }
+
+
+    private static List<String> getClassNames(String packageName) {
 
         // class loader
-       // ClassLoader classLoader = currentClass.getClassLoader() ;
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader() ;
-        URL packageResource =
-                classLoader.getResource(       // classLoader - "системный" элемент,
-                        packageName.replace(       // способный определить файловый
-                                ".",               // ресурс по имени (соотношение пакетов
-                                "/" )              // и реальных файлов). В имени ресурса
-                ) ;                            // вместо "." должны быть "/"
-        if( packageResource == null ) {
-            System.out.println( "Resource identification error" ) ;
-            return null ;
+        // ClassLoader = currentClass.getClassLoader() ;
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        URL packageResource = classLoader.getResource(       // classLoader - "системный" элемент,
+                packageName.replace(       // способный определить файловый
+                        ".",               // ресурс по имени (соотношение пакетов
+                        "/")              // и реальных файлов). В имени ресурса
+        );                            // вместо "." должны быть "/"
+        if (packageResource == null) {
+            System.out.println("Resource identification error");
+            return null;
         }
-        String packagePath = packageResource.getPath() ;  // Реальный файловый путь
-     //   System.out.println( packagePath ) ;
+        String packagePath = packageResource.getPath();  // Реальный файловый путь
+        //   System.out.println( packagePath ) ;
         // Сканнируем как папку, определяем содержимое
-        File packageBase = new File( packagePath ) ;
-        File[] list = packageBase.listFiles() ;
-        if( list == null ) {
-            System.out.println( "Error scanning directory " + packageBase ) ;
-            return null ;
+        File packageBase = new File(packagePath);
+        File[] list = packageBase.listFiles();
+        if (list == null) {
+            System.out.println("Error scanning directory " + packageBase);
+            return null;
         }
-        List<String> classNames = new ArrayList<>() ;
-        for( File file : list ) {
+        List<String> classNames = new ArrayList<>();
+        for (File file : list) {
             // задание: если имеем директорию, вывести ее состав (один уровень вложенности)
             // задание: нас интересуют только файлы типа ".class", собираем информацию о них
             //  в коллекцию; для работы с классом нужно только имя, но с учетом пакета
-         //   System.out.println( file.getName() + " " + ( file.isFile() ? "file" : "dir" ) ) ;
-            if( file.isFile() ) {
-                String filename = file.getName() ;
-                if( filename.endsWith( ".class" ) ) {
-                    classNames.add(
-                            packageName + "." +
-                                    filename.substring( 0, filename.lastIndexOf( '.' ) )
-                    ) ;
+            //   System.out.println( file.getName() + " " + ( file.isFile() ? "file" : "dir" ) ) ;
+            if (file.isFile()) {
+                String filename = file.getName();
+                if (filename.endsWith(".class")) {
+                    classNames.add(packageName + "." + filename.substring(0, filename.lastIndexOf('.')));
                 }
-            }
-            else if( file.isDirectory() ) {
-                File[] subList = file.listFiles() ;
-                if( subList != null ) {
+            } else if (file.isDirectory()) {
+                File[] subList = file.listFiles();
+                if (subList != null) {
                     for (File sub : subList) {
-                //        System.out.println( " > " + sub.getName() + " " +ConsoleColors.GREEN + ( sub.isFile() ? "file" : "dir" ) +ConsoleColors.RESET ) ;
-                        if( sub.isFile() ) {
-                            String filename = sub.getName() ;
-                            if( filename.endsWith( ".class" ) ) {
-                                classNames.add(
-                                        packageName + "." + file.getName() + "." +
-                                                filename.substring( 0, filename.lastIndexOf( '.' ) )
-                                ) ;
+                        //        System.out.println( " > " + sub.getName() + " " +ConsoleColors.GREEN + ( sub.isFile() ? "file" : "dir" ) +ConsoleColors.RESET ) ;
+                        if (sub.isFile()) {
+                            String filename = sub.getName();
+                            if (filename.endsWith(".class")) {
+                                classNames.add(packageName + "." + file.getName() + "." + filename.substring(0, filename.lastIndexOf('.')));
                             }
                         }
                     }
                 }
             }
         }
-        return classNames ;
+        return classNames;
     }
 }
 /*
