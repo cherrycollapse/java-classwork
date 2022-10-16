7b57554961034377d50dc794c89538112629fe0c
cherrycollapse <cherrycollapse@gmail.com>
2022-10-16 03:05:02 +0300

    Inject Homework

--- src/main/java/step/learning/App.java
+++ src/main/java/step/learning/App.java
@@ -19,122 +19,140 @@ import java.util.List;
 import java.util.Scanner;
 
 public class App {
-    @Inject  // ссылка на объект будет инициализирована контейнером (иньектором)
-    private StringService stringService ;
+//    @Inject  // ссылка на объект будет инициализирована контейнером (иньектором)
+//    private StringService stringService ;
+//
+//    // @Inject @Named("ten")
+//    private final RandomProvider randomProvider ;
+//    @Inject
+//    public App(@Named("ten") RandomProvider randomProvider) {
+//        // иньекция через конструктор лучше тем, что позволяет использовать final поля
+//        this.randomProvider = randomProvider;
+//    }
+//
+//    @Inject @Named("128")
+//    private HashService hash128 ;
+//    @Inject @Named("160")
+//    private HashService hash160 ;
 
-    // @Inject @Named("ten")
-    private final RandomProvider randomProvider ;
     @Inject
-    public App(@Named("ten") RandomProvider randomProvider) {
-        // иньекция через конструктор лучше тем, что позволяет использовать final поля
+    @Named("MsConnectionString")
+    private String msConnectionString;
+    @Inject
+    @Named("OracleConnectionString")
+    private String oracleConnectionString;
+
+    // Д.З. Обеспечить внедрение всех зависимостей в Арр (кроме именованных строк)
+    //через конструкторы
+    private final HashService hash128;
+    private final HashService hash160;
+    private final StringService stringService;
+    private final RandomProvider randomProvider;
+
+    @Inject
+    public App(@Named("ten") RandomProvider randomProvider, @Named("128") HashService hash128, @Named("160") HashService hash160, StringService stringService) {
         this.randomProvider = randomProvider;
+        this.stringService = stringService;
+        this.hash128 = hash128;
+        this.hash160 = hash160;
     }
 
-    @Inject @Named("128")
-    private HashService hash128 ;
-    @Inject @Named("160")
-    private HashService hash160 ;
-
-    @Inject @Named("MsConnectionString")
-    private String msConnectionString ;
-    @Inject @Named("OracleConnectionString")
-    private String oracleConnectionString ;
-
+    //     Реалізувати службу часу з методами
+//    getDate() - повертає рядок з поточною датою у форматі чч.мм.рррр
+//    getTime() - повертає рядок з поточним часом у форматі гг:хх:сс
+//    Ін'єктувати службу у App, вивести рез-ти роботи методів.
     @Inject
     DateTimeService dateTimeService;
 
     public void run() {
-        System.out.println("Date : "+dateTimeService.getDate());
-        System.out.println("Time : "+dateTimeService.getTime());
 
-//        System.out.println( "IoC Demo" ) ;
-//        System.out.println( "StringService: " + stringService.getString() ) ;
-//        System.out.println( "RandomProvider: " + randomProvider.getN() ) ;
-//        System.out.println( "HashService (128bit): " + hash128.hash( "Hello" ) ) ;
-//        System.out.println( "HashService (160bit): " + hash160.hash( "Hello" ) ) ;
-//        System.out.println( "MsConnectionString: " + msConnectionString ) ;
-//        System.out.println( "OracleConnectionString: " + oracleConnectionString ) ;
+        System.out.println("Date : " + dateTimeService.getDate());
+        System.out.println("Time : " + dateTimeService.getTime());
+
+        System.out.println("IoC Demo");
+        System.out.println("StringService: " + stringService.getString());
+        System.out.println("RandomProvider: " + randomProvider.getN());
+        System.out.println("HashService (128bit): " + hash128.hash("Hello"));
+        System.out.println("HashService (160bit): " + hash160.hash("Hello"));
+        System.out.println("MsConnectionString: " + msConnectionString);
+        System.out.println("OracleConnectionString: " + oracleConnectionString);
 
     }
 
     public void runMenu() {
         // Определяем текущий класс
-        Class<?> currentClass = Main.class ;
+        Class<?> currentClass = Main.class;
         // Извлечь из имени класса имя пакета
-        String packageName = currentClass.getPackage().getName() ;
+        String packageName = currentClass.getPackage().getName();
         // Получаем перечень имен классов из пакета и его под-пакетов (1 уровень вложенности)
-        List<String> classNames = getClassNames( packageName ) ;
-        if( classNames == null ) {
-            System.out.println( "Error scanning package" ) ;
-            return ;
+        List<String> classNames = getClassNames(packageName);
+        if (classNames == null) {
+            System.out.println("Error scanning package");
+            return;
         }
 
         // region выбрать только те классы, которые аннотированы как "DemoClass"
-        List<Class<?>> demoClasses = new ArrayList<>() ;
-        for( String className : classNames ) {
-            Class<?> theClass ;
+        List<Class<?>> demoClasses = new ArrayList<>();
+        for (String className : classNames) {
+            Class<?> theClass;
             try {
-                theClass = Class.forName( className ) ;
+                theClass = Class.forName(className);
+            } catch (Exception ignored) {  // ignored - спец. имя для неиспользуемой переменной
+                continue;
             }
-            catch( Exception ignored ) {  // ignored - спец. имя для неиспользуемой переменной
-                continue ;
-            }
-            if( theClass.isAnnotationPresent( DemoClass.class ) ) {
-                demoClasses.add( theClass ) ;
+            if (theClass.isAnnotationPresent(DemoClass.class)) {
+                demoClasses.add(theClass);
             }
             // System.out.println( className ) ;
         }
         // endregion
 
         // region Вывести меню и предложить выбор
-        System.out.println( "Demo classes:" ) ;
-        int i = 1 ;
-        for( Class<?> theClass : demoClasses ) {
-            System.out.printf( "%d. %s %n", i++, theClass.getName() ) ;
+        System.out.println("Demo classes:");
+        int i = 1;
+        for (Class<?> theClass : demoClasses) {
+            System.out.printf("%d. %s %n", i++, theClass.getName());
         }
-        System.out.println( "0. Exit" ) ;
-        System.out.print( "Make your choice: " ) ;
+        System.out.println("0. Exit");
+        System.out.print("Make your choice: ");
         // endregion
 
         // region Получить выбор, обработать (на ошибки) и запустить (EntryPoint)
-        Scanner kbScanner = new Scanner( System.in ) ;
-        int userChoice = -1 ;
+        Scanner kbScanner = new Scanner(System.in);
+        int userChoice = -1;
         try {
-            userChoice = kbScanner.nextInt() ;
-        }
-        catch( InputMismatchException ignored ) {
-            System.out.println( "Incorrect choice" ) ;
+            userChoice = kbScanner.nextInt();
+        } catch (InputMismatchException ignored) {
+            System.out.println("Incorrect choice");
         }
 
-        if( userChoice == 0 ) {
-            System.out.println( "Bye" ) ;
-            return ;
+        if (userChoice == 0) {
+            System.out.println("Bye");
+            return;
         }
-        int index = userChoice - 1 ;
-        if( index >= demoClasses.size() || index < 0 ) {
-            System.out.println( "Incorrect choice" ) ;
-        }
-        else {
-            Class<?> exeClass = demoClasses.get( index ) ;
+        int index = userChoice - 1;
+        if (index >= demoClasses.size() || index < 0) {
+            System.out.println("Incorrect choice");
+        } else {
+            Class<?> exeClass = demoClasses.get(index);
             // сканнируем методы на аннотацию EntryPoint и запускаем первый найденный
-            Method[] methods = exeClass.getDeclaredMethods() ;
-            for( Method method : methods ) {
-                if( method.isAnnotationPresent( EntryPoint.class ) ) {
+            Method[] methods = exeClass.getDeclaredMethods();
+            for (Method method : methods) {
+                if (method.isAnnotationPresent(EntryPoint.class)) {
                     try {
                         method.invoke(
                                 exeClass.getDeclaredConstructor().newInstance()
-                        ) ;
-                    }
-                    catch( Exception ex ) {
-                        System.out.println( "Execution error: " + ex.getMessage() ) ;
+                        );
+                    } catch (Exception ex) {
+                        System.out.println("Execution error: " + ex.getMessage());
                     }
 
-                    break ;
+                    break;
                 }
             }
         }
 
-        System.out.println( "Choice: " + userChoice ) ;
+        System.out.println("Choice: " + userChoice);
         // endregion
 /*
 Д.З. Обеспечить повтор диалога (меню-выбор) до выбора выхода
@@ -144,61 +162,61 @@ public class App {
    быть ранее создан, в таком случае использовать "старый" объект, новый не создавать.
  */
     }
-    private static List<String> getClassNames( String packageName ) {
+
+    private static List<String> getClassNames(String packageName) {
         // class loader
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader() ;
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
         URL packageResource =
                 classLoader.getResource(         // classLoader - "системный" элемент,
                         packageName.replace(     // способный определить файловый
                                 ".",             // ресурс по имени (соотношение пакетов
-                                "/" )            // и реальных файлов). В имени ресурса
-                ) ;                              // вместо "." должны быть "/"
-        if( packageResource == null ) {
-            System.out.println( "Resource identification error" ) ;
-            return null ;
+                                "/")            // и реальных файлов). В имени ресурса
+                );                              // вместо "." должны быть "/"
+        if (packageResource == null) {
+            System.out.println("Resource identification error");
+            return null;
         }
-        String packagePath = packageResource.getPath() ;  // Реальный файловый путь
+        String packagePath = packageResource.getPath();  // Реальный файловый путь
         // System.out.println( packagePath ) ;
         // Сканнируем как папку, определяем содержимое
-        File packageBase = new File( packagePath ) ;
-        File[] list = packageBase.listFiles() ;
-        if( list == null ) {
-            System.out.println( "Error scanning directory " + packageBase ) ;
-            return null ;
+        File packageBase = new File(packagePath);
+        File[] list = packageBase.listFiles();
+        if (list == null) {
+            System.out.println("Error scanning directory " + packageBase);
+            return null;
         }
-        List<String> classNames = new ArrayList<>() ;
-        for( File file : list ) {
+        List<String> classNames = new ArrayList<>();
+        for (File file : list) {
             // задание: если имеем директорию, вывести ее состав (один уровень вложенности)
             // задание: нас интересуют только файлы типа ".class", собираем информацию о них
             //  в коллекцию; для работы с классом нужно только имя, но с учетом пакета
             // System.out.println( file.getName() + " " + ( file.isFile() ? "file" : "dir" ) ) ;
-            if( file.isFile() ) {
-                String filename = file.getName() ;
-                if( filename.endsWith( ".class" ) ) {
+            if (file.isFile()) {
+                String filename = file.getName();
+                if (filename.endsWith(".class")) {
                     classNames.add(
                             packageName + "." +
-                                    filename.substring( 0, filename.lastIndexOf( '.' ) )
-                    ) ;
+                                    filename.substring(0, filename.lastIndexOf('.'))
+                    );
                 }
-            }
-            else if( file.isDirectory() ) {
-                File[] subList = file.listFiles() ;
-                if( subList != null ) {
+            } else if (file.isDirectory()) {
+                File[] subList = file.listFiles();
+                if (subList != null) {
                     for (File sub : subList) {
                         // System.out.println( "  " + sub.getName() + " " + ( sub.isFile() ? "file" : "dir" ) ) ;
-                        if( sub.isFile() ) {
-                            String filename = sub.getName() ;
-                            if( filename.endsWith( ".class" ) ) {
+                        if (sub.isFile()) {
+                            String filename = sub.getName();
+                            if (filename.endsWith(".class")) {
                                 classNames.add(
                                         packageName + "." + file.getName() + "." +
-                                                filename.substring( 0, filename.lastIndexOf( '.' ) )
-                                ) ;
+                                                filename.substring(0, filename.lastIndexOf('.'))
+                                );
                             }
                         }
                     }
                 }
             }
         }
-        return classNames ;
+        return classNames;
     }
 }
